ebece5c250668e4fd8015b9e40ff9112c5734109
Nimesh <nimeshg@navantis.com>
2015-11-12 17:05:11 +0530

    console host added

---
+++ GeoLib.ConsoleHost/Program.cs
@@ -0,0 +1,39 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="ABC">
+//   ABC
+// </copyright>
+// <summary>
+//   Defines the Program type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace GeoLib.ConsoleHost
+{
+    using System;
+    using System.ServiceModel;
+
+    using GeoLib.Services;
+
+    /// <summary>
+    /// The program.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// The main.
+        /// </summary>
+        /// <param name="args">
+        /// The args.
+        /// </param>
+        public static void Main(string[] args)
+        {
+            var hostGeoManager = new ServiceHost(typeof(GeoManager));
+            hostGeoManager.Open();
+
+            Console.WriteLine("Services started. Press [Enter] to");
+            Console.ReadLine();
+
+            hostGeoManager.Close();
+        }
+    }
+}
