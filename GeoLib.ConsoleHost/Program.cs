6714d878e0fcc85c29032e9fb59dae2d8379a50d
Nimesh <nimeshg@navantis.com>
2015-11-18 17:27:53 +0530

    programatically define endpoints

--- GeoLib.ConsoleHost/Program.cs
+++ GeoLib.ConsoleHost/Program.cs
@@ -11,7 +11,9 @@ namespace GeoLib.ConsoleHost
 {
     using System;
     using System.ServiceModel;
+    using System.ServiceModel.Description;
 
+    using GeoLib.Contracts;
     using GeoLib.Services;
 
     /// <summary>
@@ -28,6 +30,19 @@ namespace GeoLib.ConsoleHost
         public static void Main(string[] args)
         {
             var hostGeoManager = new ServiceHost(typeof(GeoManager));
+
+            var address = "net.tcp://localhost:3456/GeoService";
+            var addressMex = "net.tcp://localhost:3456/mex";
+            var binding = new NetTcpBinding();
+            var contract = typeof(IGeoService);
+
+            var behavior = new ServiceMetadataBehavior();
+
+            hostGeoManager.Description.Behaviors.Add(behavior);
+
+            hostGeoManager.AddServiceEndpoint(contract, binding, address);
+            hostGeoManager.AddServiceEndpoint(ServiceMetadataBehavior.MexContractName, MetadataExchangeBindings.CreateMexTcpBinding(), addressMex);
+
             hostGeoManager.Open();
 
             Console.WriteLine("Services started. Press [Enter] to exit.");
