e25995461c105d491d488beca585e94f4228927c
Nimesh <nimeshg@navantis.com>
2015-11-23 11:55:58 +0530

    added wpf host

---
+++ GeoLib.WindowsHost/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GeoLib.WindowsHost
+{
+    using System.Threading;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            BtnStart.IsEnabled = true;
+            BtnStop.IsEnabled = false;
+
+            this.Title = "UI Running on Thread " + Thread.CurrentThread.ManagedThreadId;
+        }
+
+        private void BtnStart_OnClick(object sender, RoutedEventArgs e)
+        {
+
+            BtnStart.IsEnabled = false;
+            BtnStop.IsEnabled = true;
+        }
+
+        private void BtnStop_OnClick(object sender, RoutedEventArgs e)
+        {
+            BtnStart.IsEnabled = true;
+            BtnStop.IsEnabled = false;
+        }
+    }
+}
